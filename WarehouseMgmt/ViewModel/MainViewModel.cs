b2c44022791b3f366b4723ea72f828b89e66eb46
Hồ Minh Trí <39859167+tonyho96@users.noreply.github.com>
2019-04-06 14:05:05 +0700

    Create Custom Control Bar

--- WarehouseMgmt/ViewModel/MainViewModel.cs
+++ WarehouseMgmt/ViewModel/MainViewModel.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace WarehouseMgmt.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
         public bool IsLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
         public MainViewModel()
         {
-
-            if (!IsLoaded)
-            {
-                IsLoaded = true;
-                LoginWindow loginWindow = new LoginWindow();
-                loginWindow.ShowDialog();
-            }
+            LoadedWindowCommand = new RelayCommand<object>(
+                (p) => { return true; }, (p) =>
+                {
+                    IsLoaded = true;
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.ShowDialog();
+                }
+            );
         }
     }
 }
