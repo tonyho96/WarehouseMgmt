6b26a3918d9a96da2ff3490153875ef07477e42b
Hồ Minh Trí <39859167+tonyho96@users.noreply.github.com>
2019-04-06 10:48:45 +0700

    Setup Project based on MVVM Model

---
+++ WarehouseMgmt/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WarehouseMgmt.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+        }
+    }
+}
