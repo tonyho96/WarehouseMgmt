091e292421b1f010704ebfb94defeceac8cb4cc2
Hồ Minh Trí <39859167+tonyho96@users.noreply.github.com>
2019-04-07 19:23:45 +0700

    Create Another Windows

--- WarehouseMgmt/ViewModel/MainViewModel.cs
+++ WarehouseMgmt/ViewModel/MainViewModel.cs
@@ -12,6 +12,14 @@ namespace WarehouseMgmt.ViewModel
     {
         public bool IsLoaded = false;
         public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SupplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
+
         public MainViewModel()
         {
             LoadedWindowCommand = new RelayCommand<object>(
@@ -22,6 +30,13 @@ namespace WarehouseMgmt.ViewModel
                     loginWindow.ShowDialog();
                 }
             );
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UnitWindow wd = new UnitWindow(); wd.ShowDialog(); });
+            SupplierCommand = new RelayCommand<object>((p) => { return true; }, (p) => { SupplierWindow wd = new SupplierWindow(); wd.ShowDialog(); });
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CustomerWindow wd = new CustomerWindow(); wd.ShowDialog(); });
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) => { ObjectWindow wd = new ObjectWindow(); wd.ShowDialog(); });
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UserWindow wd = new UserWindow(); wd.ShowDialog(); });
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) => { InputWindow wd = new InputWindow(); wd.ShowDialog(); });
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) => { OutputWindow wd = new OutputWindow(); wd.ShowDialog(); });
         }
     }
 }
